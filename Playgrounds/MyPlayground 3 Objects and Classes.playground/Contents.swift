3ff8967b271d4993d6caf5d0563279d1ae778f43
Islomzhan Akhmedov <ahmedovislam00@gmail.com>
2016-12-22 12:59:34 -0600

    Objects and Classes covered

--- Playgrounds/MyPlayground 3 Objects and Classes.playground/Contents.swift
+++ Playgrounds/MyPlayground 3 Objects and Classes.playground/Contents.swift
@@ -2,4 +2,30 @@
 
 import UIKit
 
-var str = "Hello, playground"
+class Ghost {
+    var isAlive = true
+    var strength = 9
+    
+    func kill() {
+        isAlive = false
+    }
+    
+    func isStrong() -> Bool{
+        if strength > 10{
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var ghost = Ghost()
+print(ghost.isAlive)
+ghost.strength = 20;
+print(ghost.strength)
+ghost.kill()
+print(ghost.isAlive)
+print(ghost.isStrong())
+
+
+
