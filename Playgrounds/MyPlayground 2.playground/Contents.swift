b8fc3244533f312a2d05a30307dfb4d55759bfd2
Islomzhan Akhmedov <ahmedovislam00@gmail.com>
2016-12-22 12:20:39 -0600

    initial start

---
+++ Playgrounds/MyPlayground 2.playground/Contents.swift
@@ -0,0 +1,43 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var i = 7;
+//while i <= 140{
+//    print(i)
+//    i+=7;
+//}
+//
+//var array = [7, 23, 98, 1, 0, 763]
+//var i = 0;
+//
+//
+//
+//
+//while i < array.count{
+//    array[i] += 1;
+//    i += 1;
+//}
+//print(array)
+//
+//
+//
+//
+var array = [8,4,8,1]
+for number in array {
+    print(number)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
