9219ca1770058224522a9a0aab23d9a748574e6a
Islomzhan Akhmedov <ahmedovislam00@gmail.com>
2016-12-24 15:38:15 -0600

    initial commit

---
+++ Playgrounds/Controlling the Keyboard/Controlling the Keyboard/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Controlling the Keyboard
+//
+//  Created by Zhan Dov on 12/24/16.
+//  Copyright © 2016 Accolade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+}
+
+
+
---
+++ Playgrounds/Controlling the Keyboard/readme.md
@@ -0,0 +1,5 @@
+### Controlling the Keyboard
+
+When user clicks to return button on keyboard, it disappears.
+
+Also, the keyboard disappears when user clicks to the space above the keyboard.
