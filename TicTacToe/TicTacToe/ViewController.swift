35310f125331071075d243bf27077267dccfe5b4
Islomzhan Akhmedov <ahmedovislam00@gmail.com>
2016-12-27 11:18:15 -0600

    version 1.0

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -12,6 +12,38 @@ class ViewController: UIViewController {
 
     
     
+    @IBOutlet weak var turnImage: UIImageView!
+    
+    @IBOutlet weak var turnLabel: UILabel!
+    
+    @IBAction func newGame(_ sender: AnyObject) {
+        activeGame = true
+        
+        activePlayer = 1
+        
+        gameState = [0,0,0,0,0,0,0,0,0]
+        
+        
+        for i in 1..<10 {
+            
+            if let button = view.viewWithTag(i) as? UIButton {
+                
+                button.setImage(nil, for: [])
+            }
+            
+            winnerLabel.isHidden = true
+            playingAgainButton.isHidden = true
+            
+            winnerLabel.center = CGPoint(x: winnerLabel.center.x - 500, y: winnerLabel.center.y)
+            playingAgainButton.center = CGPoint(x: playingAgainButton.center.x - 500, y: playingAgainButton.center.y)
+            
+        }
+        //clearing below section
+        turnImage.image = UIImage(named: "nought.png")
+        turnLabel.text = " -Turn"
+        turnLabel.backgroundColor = UIColor(red: 103/255, green: 206/255, blue: 122/255, alpha: 1)
+        turnImage.backgroundColor = UIColor(red: 103/255, green: 206/255, blue: 122/255, alpha: 1)
+    }
     @IBOutlet weak var winnerLabel: UILabel!
     @IBOutlet weak var playingAgainButton: UIButton!
     @IBAction func playAgain(_ sender: Any) {
@@ -38,6 +70,12 @@ class ViewController: UIViewController {
             
         }
         
+        //clearing below section
+        turnImage.image = UIImage(named: "nought.png")
+        turnLabel.text = " -Turn"
+        turnLabel.backgroundColor = UIColor(red: 103/255, green: 206/255, blue: 122/255, alpha: 1)
+        turnImage.backgroundColor = UIColor(red: 103/255, green: 206/255, blue: 122/255, alpha: 1)
+        
     }
     // 1 is noughts, 2 is crosses
     
@@ -50,6 +88,7 @@ class ViewController: UIViewController {
     let winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3, 6], [1,4,7], [2,5,8],
                                [0,4,8], [2,4,6]]
  
+    @IBOutlet weak var asdfda: UIButton!
     @IBAction func asdfda(_ sender: AnyObject) {
         
         let activePosition = sender.tag - 1
@@ -61,23 +100,42 @@ class ViewController: UIViewController {
             if activePlayer == 1 {
                 
                 sender.setImage(UIImage(named: "nought.png"), for: [])
+                turnImage.image = UIImage(named: "cross.png")
                 
                 activePlayer = 2
                 
             } else {
                 
                 sender.setImage(UIImage(named: "cross.png"), for: [])
+                turnImage.image = UIImage(named: "nought.png")
+                
                 activePlayer = 1
             }
             
         }
         
+        if gameState.contains(0) == false && activeGame {
+            
+            winnerLabel.text = "Nobody won"
+            playingAgainButton.isHidden = false
+            winnerLabel.isHidden = false
+            
+            UIView.animate(withDuration: 1, animations: {
+                
+                self.winnerLabel.center = CGPoint(x: self.winnerLabel.center.x + 500, y: self.winnerLabel.center.y)
+                self.playingAgainButton.center = CGPoint(x: self.playingAgainButton.center.x + 500, y: self.playingAgainButton.center.y)
+                
+            })
+            
+        activeGame = false
+            
+        }
         for combination in winningCombinations {
             
             if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
                 
                 // We have a winner!
-                
+
                 activeGame = false
                 
                 winnerLabel.isHidden = false
@@ -86,10 +144,22 @@ class ViewController: UIViewController {
                 
                 if gameState[combination[0]] == 1 {
                     
-                    winnerLabel.text = "Noughts have won"
+                    winnerLabel.text = "Noughts have won!"
+                    turnImage.image = UIImage(named: "nought.png")
+                    turnLabel.text = " is Winner!"
+                    turnLabel.adjustsFontSizeToFitWidth = true
+                    turnLabel.backgroundColor = UIColor(red: 255/255, green: 83/255, blue: 75/255, alpha: 1)
+                    turnImage.backgroundColor = UIColor(red: 255/255, green: 83/255, blue: 75/255, alpha: 1)
+    
                 } else {
                     winnerLabel.text = "Crosses have won!"
+                    turnImage.image = UIImage(named: "cross.png")
+                    turnLabel.text = " is Winner!"
+                    turnLabel.adjustsFontSizeToFitWidth = true
+                    turnLabel.backgroundColor = UIColor(red: 119/255, green: 91/255, blue: 163/255, alpha: 1)
+                    turnImage.backgroundColor = UIColor(red: 119/255, green: 91/255, blue: 163/255, alpha: 1)
                 }
+                
                 
                 
                 UIView.animate(withDuration: 1, animations: {
@@ -100,6 +170,7 @@ class ViewController: UIViewController {
                 })
                 
             }
+        
         }
         
     }
---
+++ TicTacToe/readme.md
@@ -0,0 +1,7 @@
+### Tic-Tac-Toe Game
+
+Used a slider and table view
+
+***
+
+![alt tag](https://github.com/accoladea/exploring-swift/blob/master/Tic-Tac-Toe%20Game/tic-tac-toe.png "a screenshot of the app")
