5187d9e48aac63b0463df924ff3314df404bfd95
Islomzhan Akhmedov <ahmedovislam00@gmail.com>
2016-12-27 20:40:44 -0600

    initial commit

---
+++ Playgrounds/Finding Location/Finding Location/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Finding Location
+//
+//  Created by Zhan Dov on 12/27/16.
+//  Copyright © 2016 Accolade. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation: CLLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpanMake (latDelta, lonDelta)
+        
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.map.setRegion(region, animated: true)
+        
+        
+        
+        
+        
+    }
+
+
+}
+
---
+++ Playgrounds/Finding Location/Finding LocationTests/Finding_LocationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Finding_LocationTests.swift
+//  Finding LocationTests
+//
+//  Created by Zhan Dov on 12/27/16.
+//  Copyright © 2016 Accolade. All rights reserved.
+//
+
+import XCTest
+@testable import Finding_Location
+
+class Finding_LocationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
