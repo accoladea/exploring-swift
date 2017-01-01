03dadfef7799b6a7b091a6897ece2ed8114fbd97
Islomzhan Akhmedov <ahmedovislam00@gmail.com>
2017-01-01 12:08:31 -0600

    initial commit

--- Back To Bach/Back To Bach/ViewController.swift
+++ Back To Bach/Back To Bach/ViewController.swift
@@ -7,12 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player = AVAudioPlayer()
+    let audioPath = Bundle.main.path(forResource: "bach", ofType: "mp3")
+    var timer = Timer()
+    
+    func updateScrubber() {
+        scrubber.value = Float(player.currentTime)
+    }
+    @IBAction func play(_ sender: Any) {
+        player.play()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateScrubber), userInfo: nil, repeats: true)
+    }
+    @IBAction func volumeChanged(_ sender: Any) {
+        player.volume = volumeSlider.value
+    }
+    @IBOutlet weak var volumeSlider: UISlider!
+    @IBAction func scrubberMoved(_ sender: Any) {
+        player.currentTime = TimeInterval(scrubber.value)
+    }
+    @IBOutlet weak var scrubber: UISlider!
+    @IBAction func stop(_ sender: Any) {
+        
+        scrubber.value = 0
+        timer.invalidate()
+        player.pause()
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+        }
+        catch {
+            
+        }
+        
+    }
+    @IBAction func pause(_ sender: Any) {
+        
+        player.pause()
+        
+        timer.invalidate()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        do {
+        
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            
+            scrubber.maximumValue = Float(player.duration)
+        }
+        catch {
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Back To Bach/readme.md
@@ -0,0 +1,8 @@
+### Back To Back
+
+Mozart Piano Music.
+Used AVFoundation. Plays, pauses the music and scrubber is changeable by slider.
+
+***
+
+![alt tag](https://github.com/accoladea/exploring-swift/blob/master/Back%20To%20Back/mozart-music.png "a screenshot of the app")
